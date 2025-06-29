3f1ad42d42754c3c5bf872773474c2f42450c189
Priyanshu25102011 <goldenworld201@gmail.com>
2025-06-29 16:11:43 +0530

    Initial commit

---
+++ Statements.py
@@ -0,0 +1,11 @@
+# Printing a greeting mesasge
+print("Hello, World!")
+
+# Printing my name
+print("My name is Priyanshu")
+
+# Printinga fun fact about me
+print("I love playing sports")
+
+# Performing and printing a simple calculation
+print("The result of 3+5 is:", 3 + 5)
