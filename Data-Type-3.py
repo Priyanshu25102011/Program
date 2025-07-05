49742ae908da82e860e56aac9bc9207c31861dbc
Priyanshu25102011 <goldenworld201@gmail.com>
2025-07-05 16:13:31 +0530

    add new files

---
+++ Data-Type-3.py
@@ -0,0 +1,7 @@
+text = str(input("Enter a string: "))
+
+revText = text[::-1]
+text = revText
+
+print("Reversed string:", text)
+print(text)
---
+++ Store_Birthdays.py
@@ -0,0 +1,5 @@
+Priyanshu = "25 October"
+TukTuk = "9 May"
+print("Priyanshu's birthday is on", Priyanshu)
+print("TukTuk's birthday is on", TukTuk)
+
