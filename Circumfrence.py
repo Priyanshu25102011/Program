071c05eb99252dd5b803a252bc773b488bdcdf54
Priyanshu25102011 <goldenworld201@gmail.com>
2025-08-09 16:32:57 +0530

    Add files via upload

---
+++ Circumfrence.py
@@ -0,0 +1,7 @@
+def Circumfrence(R):
+    return (2 * 22/7 * R)
+
+R = int(input("Enter the radius of the circle: "))
+area = input("Enter the size measurement (cm, m, km): ")
+print("Circumference of the circle is:", Circumfrence(R),"sq.",area,".")
+    
