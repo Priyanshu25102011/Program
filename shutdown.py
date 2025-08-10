eced2f0af4368db06834303dff21b841e38d577e
Priyanshu25102011 <goldenworld201@gmail.com>
2025-08-10 16:41:25 +0530

    Add files via upload

---
+++ shutdown.py
@@ -0,0 +1,11 @@
+command = input("do you want to shutdown (yes/no)")
+def shutdown(command):
+    if (command == "yes"):
+      print("shutting down...")
+    
+    else:
+       print("sorry!")
+
+shutdown(command)
+
+     
