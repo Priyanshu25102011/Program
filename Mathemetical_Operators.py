d3fdbc39777d38a7a7135828a4d8587fb3ea2a94
Priyanshu25102011 <goldenworld201@gmail.com>
2025-08-23 16:13:10 +0530

    Add files via upload

---
+++ Mathemetical_Operators.py
@@ -0,0 +1,7 @@
+import math
+print('THe floor and celieng value of the 23.56 are: ' + str(math.floor(23.56)) + ',' + str(math.ceil(23.56)))
+x = 10
+y = -15
+print('The value of x after copyiong the sign from y is: ' + str(math.copysign(x,y)))
+print('The absolute value of -96 and 56 are:' + str(math.fabs(-96)) + ',' + str(math.fabs(56)))
+print('The GCD of 24 and 56:' + str(math.gcd(24,56)))
