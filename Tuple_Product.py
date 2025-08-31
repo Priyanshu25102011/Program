337da9a77a816e92dea41e7fd3853eeb3c04d048
Priyanshu25102011 <goldenworld201@gmail.com>
2025-08-31 16:21:55 +0530

    Update Tuple_Product.py

--- Tuple_Product.py
+++ Tuple_Product.py
@@ -1,5 +1,5 @@
 my_tuple = (1, 2, 3, 4, 5)
-print(f"The original tuple is: {my_tuple}")
+print("The original tuple is:",my_tuple)
 
 product = 1
 
@@ -8,5 +8,6 @@ for number in my_tuple:
   
     product = product * number
 
-print(f"The product of all numbers in the tuple is: {product}")
+print("The product of all numbers in the tuple is: ",product)
+
 
