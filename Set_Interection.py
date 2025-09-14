8005fe911f4244d8e79ec761c8f14f98851cbaa0
Priyanshu25102011 <goldenworld201@gmail.com>
2025-09-14 15:56:45 +0530

    Add files via upload

---
+++ Set_Interection.py
@@ -0,0 +1,8 @@
+big_bro = {"Priyanshu","Sharma"}
+lil_bro = {"Tuktuk","Sharma"}
+print("Original set elements:")
+print(big_bro)
+print(lil_bro)
+print("\nIntersection of two said sets:")
+setz = big_bro.intersection(lil_bro)
+print(setz)
