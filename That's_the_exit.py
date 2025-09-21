5e517e71c640bc56a95c9dc36cd8c144b84d1d16
Priyanshu25102011 <goldenworld201@gmail.com>
2025-09-21 16:05:25 +0530

    Add files via upload

---
+++ That's_the_exit.py
@@ -0,0 +1,10 @@
+#Python pogram to demonstrate 
+#exit()
+for i in range (10):
+    #If the value of i becomes 
+    # 5 then the program is forced 
+    # to exit
+    if i == 5:
+        print(exit)
+        exit()
+    print(i)5
