314bbd9048a5f48726e3b203eb749220e534d28e
Priyanshu25102011 <goldenworld201@gmail.com>
2025-10-18 16:27:37 +0530

    Add files via upload

---
+++ Integer_to_Roman.py
@@ -0,0 +1,26 @@
+class Roman:
+    def int_to_roman(self, num: int) -> str:
+        val = [
+            1000, 900, 500, 400,
+            100, 90, 50, 40,
+            10, 9, 5, 4,
+            1
+        ]
+        syms = [
+            "M", "CM", "D", "CD",
+            "C", "XC", "L", "XL",
+            "X", "IX", "V", "IV",
+            "I"
+        ]
+        roman_num = ''
+        i = 0
+        while num > 0:
+            for _ in range(num // val[i]):
+                roman_num += syms[i]
+                num -= val[i]
+            i += 1
+        return roman_num
+number = int(input("Enter an integer to convert to Roman numeral: "))
+converter = Roman()
+roman_numeral = converter.int_to_roman(number)
+print(f"The Roman numeral for {number} is: {roman_numeral}")
